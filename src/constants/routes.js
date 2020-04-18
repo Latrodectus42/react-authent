9a3b26a92c813c24c3f06bb21662075dde58f4a2
Lolo Mac <lsoubrevilla@gmail.com>
2020-04-18 16:29:58 +0200

    Added routes for navigation

---
+++ src/constants/routes.js
@@ -0,0 +1,7 @@
+export const HOME = '/Home';
+export const ADMIN = '/Admin';
+export const SIGN_UP = '/SignIp';
+export const SIGN_IN = '/SignIn';
+export const LANDING = '/Landing';
+export const ACCOUNT = '/Account';
+export const PASSWORD_FORGET = '/PasswordForget';
