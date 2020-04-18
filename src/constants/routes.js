e8094e8e30b68f20a6c400da6a60b18f3b246eef
Lolo Mac <lsoubrevilla@gmail.com>
2020-04-18 18:20:04 +0200

    SignUp working

--- src/constants/routes.js
+++ src/constants/routes.js
@@ -1,6 +1,6 @@
 export const HOME = '/Home';
 export const ADMIN = '/Admin';
-export const SIGN_UP = '/SignIp';
+export const SIGN_UP = '/SignUp';
 export const SIGN_IN = '/SignIn';
 export const LANDING = '/Landing';
 export const ACCOUNT = '/Account';
